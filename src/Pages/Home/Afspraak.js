11359cbdc7b053be3df65aaff37cf9266f8733b2
SDeVuyst <silasdevuyst@hotmail.com>
2024-12-21 20:31:40 +0100

    Tip at Afspraak

--- src/Pages/Home/Afspraak.js
+++ src/Pages/Home/Afspraak.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Col, Row } from "reactstrap";
 import Cal from "@calcom/embed-react";
 
 const Afspraak = () => {
   return (
     <React.Fragment>
-      <Container id="afspraak">
-        <div className="pt-100 pb-90">
-          <h2 className="display-2 text-center mb-100 mt-n10">Maak een Afspraak</h2>
-          <Cal calLink="rick/get-rick-rolled" config={{ theme: "light" }}></Cal>
-        </div>
-      </Container>
+      <div className="pt-100 pb-90" id="afspraak">
+        <Container>
+          <h2
+            className="display-2 text-center pt-6 mb-10 mt-n10"
+            data-aos="fade"
+          >
+            Maak een Afspraak
+          </h2>
+          <Row className="justify-content-center">
+              <Col lg={7} className="col-12 text-center">
+              <p className="h4">
+                Vraag na bij uw zorgverzekeraar of u in aanmerking komt voor een vergoeding van uw voetverzorging!
+              </p>
+              </Col>
+          </Row>
+          <Row className="gh-1 gv-2 mt-40 pb-6">
+            <Cal calLink="rick/get-rick-rolled" config={{ theme: "light" }}></Cal>
+          </Row>
+           
+        </Container>
+      </div>
     </React.Fragment>
   );
 };
